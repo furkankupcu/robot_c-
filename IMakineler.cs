44226c88232f43045c46d8064a02c89a6c96c95a
furkankupcu <51542849+furkankupcu@users.noreply.github.com>
2020-02-28 22:35:39 +0300

    Composite

---
+++ IMakineler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje2mfk.Composite
+{
+    public interface IMakineler
+    {
+        string Ad { get; set; }
+        string Tür { get; set; }
+        void DetayGöster();
+
+    }
+}
+    /// <summary>
+   
+   
+  //  public class Makine : IMakineler
+   // {
+
+  //      public string Ad { get; set; }
+  //      public string Tür { get; set; }
+
+   //     public List<IMakineler> makinelers;
+
+  //      public Makine(string ad,string tür)
+   //     {
+   //         this.Ad = ad;
+    //        this.Tür = tür;
+   //         makinelers = new List<IMakineler>();
+
+    //    }
+
+    //   public void Ekle(IMakineler makineler)
+   //     {
+   //         makinelers.Add(makineler);
+
+   //     }
+
+      //  public void Sil(IMakineler makineler)
+   //     {
+
+    //        makinelers.Add(makineler);
+    //    }
+
+    //    public void DetayGöster()
+    //    {
+    //        foreach (var makineler in makinelers)
+    //        {
+                //
+     //       }
+     //   }
+
+     //   public class LeafMakine : IMakineler
+     //   {
+     //       public string Ad { get; set; }
+     //       public string Tür { get; set; }
+
+     //       public LeafMakine(string ad, string tür)
+      //      {
+      //          this.Ad = ad;
+      //          this.Tür = tür;
+      //      }
+
+      //      public void DetayGöster()
+      //      { 
+                //
+      //      }
+
+      //  }
+        /// </summary>
+   // }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ÇeşitForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace proje2mfk
+{
+    partial class ÇeşitForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(165, 106);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(191, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // ÇeşitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 520);
+            this.Controls.Add(this.listBox1);
+            this.Name = "ÇeşitForm";
+            this.Text = "ÇeşitForm";
+            this.Load += new System.EventHandler(this.ÇeşitForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
